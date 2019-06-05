86ac0b0f515ed5e353e4993e76a698a6324e8458
Sam King <sam@samking.org>
2019-06-04 23:59:01 -0700

    lesson 2 (control flow) WIP

---
+++ 02-control-flow/01-if.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -euxo pipefail
+
+# If statements are followed by a command that returns a 0 (true) or nonzero
+# (false) exit code.
+# Using the semicolon after your statement lets you put `then` on the same line.
+# Otherwise, you would need to put `then` on the next line.
+# A high level overview of if/else is at
+# http://mywiki.wooledge.org/BashGuide/TestsAndConditionals
+if ls . ; then
+    echo "We were successfully able to list the files in the current directory"
+fi
+
+# You can also use else.
+if ls ./non-existant-directory ; then
+    echo "This shouldn't happen"
+else
+    echo "We were unable to list the files in a non-existant directory"
+fi
+
+# A lot of the time, you'll want to do things that are more complicated than
+# that.  In those cases, you probably want to use either `[` (an alias for the
+# legacy `test` command) or `[[` (a better version of `[` that is usually what
+# you want).
+# Documentation on `[` is at:
+# http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
+# Documentation on `[[` and the differences between `[[` and `[` is at:
+# http://mywiki.wooledge.org/BashFAQ/031
+# Because most folks should only ever need to use `[[`, all of the remaining
+# conditions will use that style of test.  That said, `[[` supports some of the
+# syntax from `[`, so the `[` documentation is still useful.
+if [[ 'hello world' =~ h.*d ]] ; then
+    echo "Our regular expression test worked"
+fi
+if [[ 1 < 2 ]] ; then
+    echo "Math works"
+fi
+if [[ -e /bin ]] ; then
+    echo "/bin is a directory"
+fi
+if [[ ! 'one string' == 'two string' ]] ; then
+    echo "Those strings are not equal."
+fi
+if [[ 'one string' != 'two string' ]] ; then
+    echo "You can do 'not equal' in the ways you expect."
+fi
---
+++ 02-control-flow/README.md
@@ -0,0 +1,6 @@
+These files outline a few different Bash basics including control flow and
+variables.  There will be links to more documentation where relevant.
+
+Once you have gone through the files in this directory, make a bash script with
+a function that prints out the longest filename in the directory and returns the
+length.
