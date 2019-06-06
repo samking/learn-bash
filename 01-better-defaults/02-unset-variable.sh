c243fe57a241eb820569b59eda65f9e5934af6fa
Sam King <sam@samking.org>
2019-06-05 22:14:05 -0700

    Explicitly mention subshells

--- 01-better-defaults/02-unset-variable.sh
+++ 01-better-defaults/02-unset-variable.sh
@@ -11,6 +11,10 @@ echo "Setting a variable to the number of files in /usr/bin..."
 # Then, use tail to remove the first line, which lists a total, to avoid an
 # off-by-one.
 # Then, count the number of lines.
+# Note: $( ... ) creates a subshell and executes commands in it.  It's pretty
+# commonly used when you want to do something and then assign the results to a
+# variable.  You might see ` ... ` syntax for subshells as well, but $( ... )
+# syntax is more preferred now.
 NUM_FILES=$(ls -l /usr/bin | tail -n +2 | wc -l)
 echo "Number of files in /usr/bin: ${NUM_FILEZ}"
 echo "If there wasn't a number above, that means this script silently failed!"
